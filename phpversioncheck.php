5c3b52dff0b6ccdb5fa8763209e8cfd8fa0886fb
bill <tutnunkimpo@naver.com>
2017-02-22 23:07:25 +0900

    if you want to upgrad php7 . have to change appach2>conf>httpd.conf . and php folder

---
+++ phpversioncheck.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <title>phpversion</title>
+  </head>
+  <body>
+    <?php
+      echo '<p>PHP current version is ' . phpversion() . '</p>';
+    ?>
+  </body>
+</html>
